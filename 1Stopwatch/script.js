d41281ac6b9fc37d90759b2a5cc5f8a448aa6456
HastagKc <chaudharykshittiz950@gmail.com>
2022-12-26 17:44:35 +0545

    different Between selector and selector all

--- 1Stopwatch/script.js
+++ 1Stopwatch/script.js
@@ -1,2 +1,18 @@
 var content = document.querySelector(".class");
 content.textContent = "This will change";
+
+var idContent = document.querySelector("#selectId");
+idContent.innerHTML = "This will change id content";
+
+var getByid = document.getElementById("getElementByid");
+getByid.innerHTML = "This change get Element by id ";
+
+//! Selector Vs SelectorAll
+
+const nodeList = document.querySelectorAll(".example");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].style.backgroundColor = "blue";
+}
+
+// let node = document.querySelector(".example");
+// node.style.backgroundColor = "blue";
