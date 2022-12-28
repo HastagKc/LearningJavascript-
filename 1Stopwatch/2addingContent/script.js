2b65ab8e48a239616866a4471f0db82d55b242c8
HastagKc <chaudharykshittiz950@gmail.com>
2022-12-28 23:09:11 +0545

    dec28

--- 1Stopwatch/2addingContent/script.js
+++ 1Stopwatch/2addingContent/script.js
@@ -2,6 +2,6 @@ let add_text = document.querySelector(".add_content");
 let text_content = document.querySelector(".content");
 
 add_text.addEventListener("click", function () {
-  // adding content to the document
+  //! adding content to the document
   text_content.textContent = text_content.textContent + " Namsate";
 });
