b167c19a5161fc6f52c7c034e24d4ce41f4923e6
HastagKc <chaudharykshittiz950@gmail.com>
2024-01-18 14:33:28 +0545

    class03

---
+++ class03.js
@@ -0,0 +1,22 @@
+// ? variable and contant 
+// variable is a named location (memory address ) that holds value 
+// declaration of variable 
+
+const accountNumber = 123456789;
+var accountHolderName ="Kshittiz";
+let password ="123";
+console.log(typeof(password));
+accountHolderAddress ="Kathmandu";
+
+console.table([accountNumber,accountHolderName, password, accountHolderAddress]);
+
+// constant can't be change after declaration 
+//accountNumber=4566666; // this is not allowed 
+
+password=45;
+console.log(password);
+console.log(typeof(password));
+
+//1. var - function scope 
+// 2. both 
+// let and const are block scope 
