26439054cc6df39db94b2a39da30b5fbebc739f7
HastagKc <chaudharykshittiz950@gmail.com>
2024-01-18 17:43:48 +0545

    datatypes in hs

---
+++ 01_basics/04_datatypes.js
@@ -0,0 +1,37 @@
+/*
+
+<------------Dynamic Type----------------------------->
+
+JavaScript is a dynamic language with dynamic types. 
+Variables in JavaScript are not directly associated 
+with any particular value type, and any variable can 
+be assigned (and re-assigned) values of all types:
+
+*/
+
+let dynamicTypeExample="Harry";  // string 
+console.log(typeof dynamicTypeExample);
+
+dynamicTypeExample=22; // not its a number 
+console.log(typeof dynamicTypeExample);
+
+dynamicTypeExample=false; // now it is a boolean data types 
+console.log(typeof dynamicTypeExample);
+
+/*
+
+<----------------------Weak Typed -------------------------------------->
+
+JavaScript is also a weakly typed language, which means it allows
+implicit type conversion when an operation 
+involves mismatched types, instead of throwing type errors.
+
+<--------------type coercion --------------------------------->
+
+*/
+
+
+
+
+
+
