e4ce18ef2e6c55f4fb0aad4f8e069c7cad0c6b2e
HastagKc <chaudharykshittiz950@gmail.com>
2024-01-25 01:50:24 +0545

    color code is added

--- 02_Projects/Project3-Random-Color-changer/app.js
+++ 02_Projects/Project3-Random-Color-changer/app.js
@@ -3,6 +3,7 @@
 
 let col_change_btn = document.getElementById("btn-color-change");
 let back_col_div = document.getElementById("bg-col");
+let color_code = document.getElementById("color-code");
 
 // let create array of different colors
 let col_array = ["#7CB8B3", "#F5795D", "#324258", "#000000", "#D3C4B6"];
@@ -15,6 +16,10 @@ let changeColorFun = function () {
   //   console.log(index_number);
   //   console.log(typeof index_number);
   back_col_div.style.backgroundColor = col_array[index_number];
+  color_code.innerHTML = col_array[index_number];
 };
 
 col_change_btn.addEventListener("click", () => changeColorFun());
+
+back_col_div.style.backgroundColor = "#212121";
+color_code.innerHTML = "#212121";
