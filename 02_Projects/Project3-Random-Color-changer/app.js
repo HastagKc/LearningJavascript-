ed2a72fb1f7ef28fcc470add7a0c99c3cd7240b5
HastagKc <chaudharykshittiz950@gmail.com>
2024-01-25 01:37:07 +0545

    random color changer

---
+++ 02_Projects/Project3-Random-Color-changer/app.js
@@ -0,0 +1,20 @@
+// getElementByClassName returns collections
+// getElementById returns single value
+
+let col_change_btn = document.getElementById("btn-color-change");
+let back_col_div = document.getElementById("bg-col");
+
+// let create array of different colors
+let col_array = ["#7CB8B3", "#F5795D", "#324258", "#000000", "#D3C4B6"];
+
+// console.log(col_array[0], col_array.length);
+// creat function
+
+let changeColorFun = function () {
+  let index_number = Math.floor(Math.random() * col_array.length);
+  //   console.log(index_number);
+  //   console.log(typeof index_number);
+  back_col_div.style.backgroundColor = col_array[index_number];
+};
+
+col_change_btn.addEventListener("click", () => changeColorFun());
