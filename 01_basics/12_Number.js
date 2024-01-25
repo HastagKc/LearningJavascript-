5a759576966910483a1e8ad5619b1bf3bbaee645
HastagKc <chaudharykshittiz950@gmail.com>
2024-01-25 06:57:51 +0545

    Math and number in js

---
+++ 01_basics/12_Number.js
@@ -0,0 +1,20 @@
+const num = 9000;
+console.log(num);
+console.log(typeof num); // number
+console.log(isNaN(num)); // false
+
+// converting number into string
+let anotherNum = num.toString();
+console.log(typeof anotherNum);
+console.log(anotherNum.length);
+
+// to fixed method
+const num2 = 10.09098;
+console.log(num2.toFixed(2));
+
+// convert string value into integer or floating-point number
+let stringToNumber = parseInt(anotherNum);
+console.log(typeof stringToNumber);
+
+const num3 = 100000000000;
+console.log(num3.toLocaleString("en-IN"));
