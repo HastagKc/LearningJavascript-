0e99c779a261467cbe2474d0d9e4155fa0827e89
HastagKc <chaudharykshittiz950@gmail.com>
2024-01-27 18:10:27 +0545

    Array in js

---
+++ 02_basics/01_array.js
@@ -0,0 +1,22 @@
+// * array in js
+// Array is a data structure in programming languages
+// that is used to store and organize a collection of elements
+
+// * Array is js is resizeable
+
+// Array in js
+// 1. each element can be different types
+// 2. Array element accessed using non-negative number (Index)
+// 3. Array index start form 0 and last element is array length -1
+// 4. Javascript operation create shallow copies of an array
+
+// ----Shallow Copies-------------------??????????
+/*
+
+*Shallow Copy:*
+Making a shallow copy of an object is like copying a list of items by referencing them. Changes to one item affect both the original and the copy because they share the same references.
+
+*Deep Copy:*
+Creating a deep copy involves making a new set of items with the same names. Changes to one item do not affect the other because they are independent copies.
+
+*/
